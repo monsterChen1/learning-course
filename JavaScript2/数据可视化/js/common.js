22191728297239032e08737e7d6638893d9425b1
JackenSoul <37260532+SoulChen1@users.noreply.github.com>
2025-05-07 19:02:54 +0800

    数据可视化项目的创建

---
+++ JavaScript2/数据可视化/js/common.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        document.body.classList.add('sidenav-pinned');
+        document.body.classList.add('ready');
+    }, 200);
+});
