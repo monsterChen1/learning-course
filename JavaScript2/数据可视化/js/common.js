95d460562a98b8243913452a7bec411d92f28a00
JackenSoul <37260532+SoulChen1@users.noreply.github.com>
2025-05-09 10:04:39 +0800

    请求和响应拦截器

--- JavaScript2/数据可视化/js/common.js
+++ JavaScript2/数据可视化/js/common.js
@@ -4,3 +4,32 @@ document.addEventListener('DOMContentLoaded', () => {
         document.body.classList.add('ready');
     }, 200);
 });
+
+// 统一配置请求链接前缀
+axios.defaults.baseURL = 'http://ajax-api.itheima.net';
+
+// 请求拦截器
+axios.interceptors.request.use(
+    (config) => {
+        let token = localStorage.getItem('token');
+        if(token){
+            config.headers.Authorization = token;
+        }
+
+        return config;
+    },
+    (err) => Promise.reject(err)
+);
+
+// 响应拦截器
+axios.interceptors.response.use(
+    (response) => response.data,
+    (err) => {
+        if(err.response.status === 401){
+            localStorage.clear();
+            location.href = './login.html';
+        }
+
+        return Promise.reject(err);
+    }
+)
