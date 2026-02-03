71d36ab784ea00bc487fa863b19d19f2318c902e
JackenSoul <37260532+SoulChen1@users.noreply.github.com>
2026-02-03 18:32:09 +0800

    vue-day04

---
+++ JavaScript/Vue/day04/demo/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <h1>组件导入</h1>
+
+    <div class="box">
+      <h2>标题 <button @click="toClick()">{{ show ? '折叠' : '展开' }}</button></h2>
+      <ul v-show="show">
+        <li>xxxxxxxxxxxxxxxxxxxx</li>
+        <li>xxxxxxxxxxxxxxxxxxxx</li>
+        <li>xxxxxxxxxxxxxxxxxxxx</li>
+        <li>xxxxxxxxxxxxxxxxxxxx</li>
+      </ul>
+    </div>
+    <Draw :show="show1"></Draw>
+  </div>
+</template>
+
+<script>
+import Draw from './components/draw.vue'
+export default {
+  name: 'App',
+  components: {
+    Draw
+  },
+  data() {
+    return {
+      show: true,
+      show1: true
+    }
+  },
+  methods: {
+    toClick() {
+      this.show = !this.show
+    }
+  },
+  computed: {
+    
+  },
+  watch: {
+
+  }
+}
+</script>
+
+<style>
+.box {
+  border: 1px solid black;
+}
+</style>
---
+++ JavaScript/Vue/day04/demo/src/components/draw.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="draw">
+    <div class="box">
+      <h2>标题 <button @click="show==!show">{{ show ? '折叠' : '展开' }}</button></h2>
+      <ul v-show="show">
+        <li>xxxxxxxxxxxxxxxxxxxx</li>
+        <li>xxxxxxxxxxxxxxxxxxxx</li>
+        <li>xxxxxxxxxxxxxxxxxxxx</li>
+        <li>xxxxxxxxxxxxxxxxxxxx</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Draw',
+    props: {
+        show: Boolean
+    },
+}
+</script>
+
+<style>
+.box {
+  border: 1px solid black;
+}
+</style>
