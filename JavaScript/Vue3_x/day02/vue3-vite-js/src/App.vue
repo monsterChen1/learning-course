430ede2607af965690bb9dfb6d402e8b48060da9
JackenSoul <37260532+SoulChen1@users.noreply.github.com>
2026-09-04 10:57:46 +0800

    vue3day2

---
+++ JavaScript/Vue3_x/day02/vue3-vite-js/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+const title = '小滴课堂';
+const titleHtml = '<h1>小滴课堂</h1>'
+
+const id = "div1";
+const isDisabled = true;
+const url = "https://www.xdclass.net";
+</script>
+
+<template>
+  <div class="content">
+    <!-- 插值语法 {{  }} -->
+    <div class="a">小滴课堂</div>
+    <div class="a">{{ title }}</div>
+    <div>{{ titleHtml }}</div>
+    <div v-html="titleHtml"></div>
+
+    <!-- 属性绑定 v-bind -->
+    <div v-bind:id="id">1</div>
+    <!-- 属性绑定 简写 -->
+    <div :id="id">2</div>
+    <button :disabled="isDisabled">禁用按钮</button><br>
+    <a :href="url" target="_blank">小滴课堂</a>
+  </div>
+</template>
+
+<style scoped>
+.a {
+  color: red;
+}
+</style>
