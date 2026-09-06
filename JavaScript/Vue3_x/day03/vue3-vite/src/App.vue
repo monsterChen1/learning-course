b245cdefbfc03bb6e586a2e920bc4d1130558e02
monster <870964443@qq.com>
2026-09-07 00:16:12 +0800

    vue3-reactive

--- JavaScript/Vue3_x/day03/vue3-vite/src/App.vue
+++ JavaScript/Vue3_x/day03/vue3-vite/src/App.vue
@@ -1,13 +1,11 @@
 <script setup>
-import { ref, nextTick, shallowRef } from 'vue'
+import { ref, nextTick, shallowRef, reactive, shallowReactive } from 'vue'
 
 let count = ref(0)
-
 let obj = ref({
   name: '张三',
   age: 18
 });
-
 let obj2 = shallowRef({
   count: 0
 });
@@ -20,6 +18,14 @@ const handleClick = async () => {
   await nextTick();
   console.log(document.getElementById('box').textContent)
 }
+
+let obj3 = reactive({
+  count:0
+})
+let obj4 = shallowReactive({
+  count:0,
+  obj: { count: 0 }
+})
 </script>
 
 <template>
@@ -28,6 +34,8 @@ const handleClick = async () => {
     <button @click="handleClick">增加计数</button>
     <div @click="obj.age++">{{ obj.age }}</div>
     <div @click="obj2.count++">{{ obj2.count }}</div>
+    <div @click="obj3.count++">{{ obj3.count }}</div>
+    <div @click="obj4.obj.count++">{{ obj4.obj.count }}</div>
   </div>
 </template>
 
